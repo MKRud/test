0a789dbeaa0b0c4ea5fdb069145795f9693d2af2
MKRud <maxrud6919@gmail.com>
2020-05-14 00:54:33 +0300

    1

---
+++ index.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+var objShazam = {};
+
+request('https://www.shazam.com/shazam/v3/ru/UA/web/-/tracks/world-chart-world?pageSize=200&startFrom=0', (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+
+        var jsonObj = JSON.parse(html);
+
+        jsonObj = jsonObj['tracks'];
+       
+        for (keyFirst in jsonObj) {
+            const name = jsonObj[keyFirst]['share']['subject'];
+            const shazam = jsonObj[keyFirst]['share']['href'];
+
+            keyFirstN = Number(keyFirst) + 1;
+
+            objShazam[keyFirstN] = {
+                [name]: {
+                    shazam
+                }
+            }
+        }
+    }
+    console.log(objShazam);
+})
